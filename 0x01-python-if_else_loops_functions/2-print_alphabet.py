838ecba9674e9fec42e4e9660967ecab499e8f42
zebbyG <zebbynjau@gmail.com>
2022-05-31 02:19:03 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3  
-for z in range(97, 122):
-    if (z == 101) or (z == 113):
+for z in range(97, 123):
+    if (z == 101) and (z == 113):
        continue
     print("{:c}".format(z), end="")
