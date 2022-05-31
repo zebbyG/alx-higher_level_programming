dfbb95b08986a4253e949df291043f656a78bccd
zebbyG <zebbynjau@gmail.com>
2022-05-31 02:51:11 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3  
-for m in range(97, 122):
-    if (m == 101) or (m == 113):
-       continue
-    print("{:c}".format(m), end="")
+#!/usr/bin/python3
+for m in range(97, 123):
+    if (m != 101 and m != 113):
+        print("{}".format(chr(m)), end='')
