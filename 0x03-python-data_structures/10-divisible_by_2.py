76d73748f9a92a33e5ba87fd9c214eab231a911b
zebbyG <zebbynjau@gmail.com>
2022-06-06 10:06:44 -0700

    3-print_reversed_list_integer.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    multiples = []
-    for z in range(len(my_list) - 1):
-        if my_list[z] % 2 == 0:
-            multiples.append(True)
-        else:
-            multiples.append(False)
-            return multiples
