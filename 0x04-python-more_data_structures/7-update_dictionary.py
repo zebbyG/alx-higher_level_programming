c19680d4bee376318bd139538d56863c6c6c05eb
zebbyG <99161685+zebbyG@users.noreply.github.com>
2022-06-08 20:23:09 +0300

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,7 +3,7 @@ def update_dictionary(a_dictionary, key, value):
     if key not in a_dictionary:
         a_dictionary[key] = value
     else:
-        for i in a_dictionary:
-            if i == key:
-                a_dictionary[i] = value
+        for z in a_dictionary:
+            if z == key:
+                a_dictionary[z] = value
     return a_dictionary
