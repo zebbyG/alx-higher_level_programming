0d0c798161588cc6e821c89c89ddb58ce8abf467
zebbyG <zebbynjau@gmail.com>
2022-06-08 09:29:32 -0700

    fix task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    ret = []
-    for z in set(my_list):
-        ret += z
-    return (ret)
+    new = set(my_list)
+    res = 0
+    for i in new:
+        res += i
+    return res
