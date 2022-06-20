9aa30a4d62d4462abf315f21971335be2cd47365
email <zebbyG>
2022-06-20 13:43:10 +0300

    add task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    ncount = 0
+    for z in range(0, x):
+        try:
+            print("{:d}".format(my_list[z]), end="")
+            ncount += 1
+        except (ValueError, TypeError, IndexError):
+            pass
+    print()
+    return ncount
