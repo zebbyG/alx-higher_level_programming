418717fc7506b5329845b11f1d567f44e00f45f5
email <zebbyG>
2022-06-20 14:30:20 +0300

    remove file 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def safe_print_division(a, b):
-    try:
-        divide = a / b
-    except (ZeroDivisionError, FloatingPointError):
-        divide = None
-    finally:
-        print("inside result: {}".format(divide))
-    return divide
