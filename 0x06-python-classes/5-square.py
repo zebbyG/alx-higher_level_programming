1e090e1419f6ffad50476afab30963052b5f8f51
email <zebbyG>
2022-06-21 18:22:30 +0300

    add task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" create a class Square with attribute size assigned to value 0"""
+
+
+class Square:
+    """ Created a class Square with attribute size assigned to value zero"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        size = value
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for z in range(self.__size):
+            print("#" * self.__size)
