f6f36e9bd5752f12cd3d210c338422684542184e
email <zebbyG>
2022-06-21 13:40:06 +0300

    add task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" create a class Square with attribute size assigned to value 0"""
+
+
+class Square:
+    """ Created a class Square with attribute size assigned to value zero"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
