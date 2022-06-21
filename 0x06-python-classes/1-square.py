f66ccbefd3c8a0abb2eba6dca7a91e10b7cc0a8d
email <zebbyG>
2022-06-21 13:04:49 +0300

    add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" create a class Square with attribute size"""
+
+
+class Square:
+    """ Created a class Square with attribute size"""
+    def __init__(self, size):
+        self.__size = size
