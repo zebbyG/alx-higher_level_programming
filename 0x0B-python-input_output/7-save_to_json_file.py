bb3bf4cc236fb28f2acb5597887933aab27a9d83
zebbyG <99161685+zebbyG@users.noreply.github.com>
2022-07-06 10:40:42 +0300

    Update 7-save_to_json_file.py

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""Module 9-add_item.
-Adds all arguments to a Python list,
-and then save them to a file.
+"""Module 7-save_to_json_file.
+Writes an Object to a text file,
+using a JSON representation.
 """
 
-import sys
+
 import json
-import os.path
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-my_file = 'add_item.json'
+def save_to_json_file(my_obj, filename):
+    """Writes the representation of my_obj
+    to filename.
 
-my_list = []
+    Args:
+        - my_obj: object to write
+        - filename: file to write into
+    """
 
-if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
-    my_list = load_from_json_file(my_file)
-
-if len(sys.argv) > 1:
-    for elem in sys.argv[1:]:
-        my_list.append(elem)
-
-save_to_json_file(my_list, my_file)
+    with open(filename, 'w+') as f:
+        json.dump(my_obj, f)
