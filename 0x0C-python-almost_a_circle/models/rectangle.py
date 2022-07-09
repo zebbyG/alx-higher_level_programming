dca89bb89a44b0ee57e7b0bafec00bf28a865ffb
email <zebbyG>
2022-07-09 13:21:10 +0300

    add task

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+
+
+"""creating class Rectangle inheriting Base"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initialization function for class Rectangle"""
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """getter for width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """getter for height"""
+        return self.__height
+
+    @property
+    def x(self):
+        """getter for x"""
+        return self.__x
+
+    @property
+    def y(self):
+        """getter for y"""
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        width = value
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        height = value
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = height
+
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        x = value
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must must be >= 0")
+        self.__x = x
+
+    @y.setter
+    def y(self, value):
+        """y setter"""
+        y = value
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
+
+    def area(self):
+        return self.__width * self.__height
