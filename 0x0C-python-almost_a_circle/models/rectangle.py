73d3ac1d4be6ea76673a89a207ec3b868d514620
email <zebbyG>
2022-07-09 14:27:05 +0300

    6-main.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,8 +81,13 @@ class Rectangle(Base):
 
     def display(self):
         """method for printing Rectangle in '#' """
+        for e in range(self.__y):
+            print("")
         for z in range(self.__height):
+            for b in range(self.__x):
+                print(" ", end="")
             print("#" * self.__width)
 
     def __str__(self):
+        """method to return the Rectangle"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
