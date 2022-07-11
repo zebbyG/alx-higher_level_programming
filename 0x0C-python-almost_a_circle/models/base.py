f590c970c08a8b4e0522b996c5c9c5af6fec30f5
email <zebbyG>
2022-07-11 13:54:42 +0300

    14-main.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 
 """creating class base"""
 
+import json
+
 
 class Base:
     __nb_objects = 0
@@ -15,3 +17,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """return jason representation"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+
+        return json.dumps(list_dictionaries)
