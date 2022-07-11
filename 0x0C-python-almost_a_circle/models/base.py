6b68048bc3c67f99e5fd3dfd58c94dffea783c8b
email <zebbyG>
2022-07-11 13:56:21 +0300

    base.py task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,7 +20,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """return jason representation"""
+        """return json representation of list_dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
 
