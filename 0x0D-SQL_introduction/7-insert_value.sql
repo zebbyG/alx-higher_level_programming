9352ff2330335456349f0ce52d51edddfc1f6410
email <zebbyG>
2022-08-09 21:50:23 +0300

    add task 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- First add .
 -- A script that inserts a new a row in the table first_table in your MySQL
-INSERT INTO `first_table`(`id`, `name`) VALUES (89, Best School);
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Best School');
