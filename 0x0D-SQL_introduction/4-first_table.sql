70250c71d5cb25461d98ebbffa6d1ab0dc55181a
email <zebbyG>
2022-08-09 18:21:54 +0300

    add task 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,3 @@
 -- first table
 -- A script that creates a table in current database in MySQL
-CREATE TABLE IF EXISTS first_table (
-	id INT,
-	name VARCHAR(256),
-);
+CREATE TABLE IF EXISTS first_table(id INT, name VARCHAR(256));
