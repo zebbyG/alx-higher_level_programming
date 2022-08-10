cfdec979613e064391b006fd82d962bc711b8477
email <zebbyG>
2022-08-10 15:51:28 +0300

    remove

--- 0x0E-SQL_more_queries/1-create_user.sql
+++
@@ -1,4 +0,0 @@
--- Root user
--- A script that craetes the MySQL server
-CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd`;
-GRANT ALL PRIVILEGES ON * . * TO `user_0d_1`@`localhost`;
