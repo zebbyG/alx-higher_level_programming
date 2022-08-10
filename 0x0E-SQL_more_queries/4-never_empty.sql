d3a70c9f3d3bd330b54e712d6139eda52a58fed7
email <zebbyG>
2022-08-10 17:26:23 +0300

    add task 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- ID can't be null
 -- A script that creates the table id_not_null on MySQL server
-CREATE TABLE IF NOT EXISTS id_not_null (id, name)VALUE(INT 1, VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT (1), name VARCHAR(256));
