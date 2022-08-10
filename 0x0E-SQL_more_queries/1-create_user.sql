eb2f776fb9db4ab755253ea285e6fa3b433da5b9
email <zebbyG>
2022-08-10 15:36:54 +0300

    add task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,3 +2,4 @@
 -- A script that craetes the MySQL server
 CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd`;
 GRANT ALL PRIVILAGES ON *.* TO `user_0d_1`@`localhost`;
+FLUSH PRIVILEGES;
