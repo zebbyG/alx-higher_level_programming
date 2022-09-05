75a899b553438a3ee1d8d17720409f074dba7145
email <zebbyG>
2022-09-05 23:02:45 +0300

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let z = 'C is fun'
-let e = 'Python is cool'
-let b = 'JavaScript is amazing'
-console.log(z)
-console.log(e)
-console.log(b)
+let z = 'C is fun';
+let e = 'Python is cool';
+let b = 'JavaScript is amazing';
+console.log(z);
+console.log(e);
+console.log(b);
