50630a8aadffa2f8cdba28743d27a997f7230545
email <zebbyG>
2022-09-23 17:12:21 +0300

    sga

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Python script Fetches https://intranet.hbtn.io/status."""
-if __name__ == "__main__":
-    import urllib.request
+"""fetches https://intranet.hbtn.io/status"""
+from urllib import request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
